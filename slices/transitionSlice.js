6d7d11878904706d4bf9e730b25651b6b9a9ced7
Megan Clapinski <142531799+meganclapinski25@users.noreply.github.com>
2026-03-05 12:18:31 -0800

    trying to create slice

---
+++ slices/transitionSlice.js
@@ -0,0 +1,26 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+
+export const loadTransactions = createAsyncThunk(
+    'transactions/load', async()=>{
+        const data = await AsyncStorage.getItem('transactions');
+        return data ? JSON.parse(data) : [];
+    }
+);
+
+const transactionsSlice = createSlice({
+    name: 'transactions',
+    initialState: {
+        items: [],
+        status: 'idle', // idle | loading | succeeded | failed
+        error: null,
+      },
+})
+
+
+
+
+
+export const { addTransaction } = transactionsSlice.actions;
+export default transactionsSlice.reducer;
